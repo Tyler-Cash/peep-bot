c3b0c94024b92f55d1181246efa5bd2eca6ed505
Tyler <12643375+Tyler-Cash@users.noreply.github.com>
2024-10-14 22:49:33 +1100

    Remove description from being included in Google calendar link

--- backend/src/test/java/dev/tylercash/event/global/GoogleCalendarServiceTest.java
+++ backend/src/test/java/dev/tylercash/event/global/GoogleCalendarServiceTest.java
@@ -23,8 +23,8 @@ class GoogleCalendarServiceTest {
 
     public static Stream<Arguments> calendarTestEvents() {
         return Stream.of(
-                new Event(0, 0, 0, "Simple event 😊", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. :) 😊", time),
-                new Event(0, 0, 0, "Simple event", "small description", time)
+                new Event(0, 0, 0, "Simple event 😊", "", time),
+                new Event(0, 0, 0, "Simple event", "", time)
         ).map(Arguments::of);
     }
 
@@ -38,9 +38,7 @@ class GoogleCalendarServiceTest {
                 UriComponentsBuilder.fromHttpUrl("https://www.google.com/calendar/event" +
                                 "?action=TEMPLATE" +
                                 "&text=" + event.getName() +
-                                "&location=" + event.getLocation() +
-                                "&dates=20240816T132400Z/20240816T142400Z" +
-                                "&details=" + event.getDescription())
+                                "&dates=20240816T132400Z/20240816T142400Z")
                         .encode()
                         .build()
                         .toUri()
