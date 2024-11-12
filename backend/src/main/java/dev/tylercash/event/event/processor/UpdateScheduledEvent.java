75d27ee20ffd9877ef583cc6d3d3b6b8823ebad1
Tyler Cash <12643375+Tyler-Cash@users.noreply.github.com>
2024-11-12 22:37:57 +1100

    Remove scheduled message update. Probably not needed

--- backend/src/main/java/dev/tylercash/event/event/processor/UpdateScheduledEvent.java
+++ backend/src/main/java/dev/tylercash/event/event/processor/UpdateScheduledEvent.java
@@ -1,20 +1,20 @@
-package dev.tylercash.event.event.processor;
+// package dev.tylercash.event.event.processor;
 
-import dev.tylercash.event.db.repository.EventRepository;
-import dev.tylercash.event.discord.DiscordService;
-import dev.tylercash.event.event.model.Event;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Component;
+// import dev.tylercash.event.db.repository.EventRepository;
+// import dev.tylercash.event.discord.DiscordService;
+// import dev.tylercash.event.event.model.Event;
+// import lombok.extern.log4j.Log4j2;
+// import org.springframework.stereotype.Component;
 
-@Log4j2
-@Component
-public class UpdateScheduledEvent extends ScheduledEventProcessor {
-    public UpdateScheduledEvent(DiscordService discordService, EventRepository eventRepository) {
-        super(discordService, eventRepository);
-    }
+// @Log4j2
+// @Component
+// public class UpdateScheduledEvent extends ScheduledEventProcessor {
+//     public UpdateScheduledEvent(DiscordService discordService, EventRepository eventRepository) {
+//         super(discordService, eventRepository);
+//     }
 
-    @Override
-    public void processEvent(Event event) {
-        discordService.updateEventMessage(event);
-    }
-}
+//     @Override
+//     public void processEvent(Event event) {
+//         discordService.updateEventMessage(event);
+//     }
+// }
