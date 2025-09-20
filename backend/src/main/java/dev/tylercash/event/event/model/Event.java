6cbaf6d1a197a0905eaec53546cab01db790d121
Tyler <12643375+Tyler-Cash@users.noreply.github.com>
2025-09-20 22:09:10 +1000

    Allow maybeConfirmationMessageId to be null, should probably be using refs... anyways...

--- backend/src/main/java/dev/tylercash/event/event/model/Event.java
+++ backend/src/main/java/dev/tylercash/event/event/model/Event.java
@@ -29,8 +29,8 @@ public class Event {
     @NotNull
     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private long messageId;
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
-    private long maybeConfirmationMessageId;
+    @Schema(nullable = true, accessMode = Schema.AccessMode.READ_ONLY)
+    private Long maybeConfirmationMessageId;
     @NotNull
     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private long serverId;
